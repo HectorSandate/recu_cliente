2a80177bdac58bddd104b4c83e65253a3d14598b
HectorSandate <hectorjosediazsandate@gmail.com>
2025-06-06 15:10:25 -0600

    examen recu cliente

---
+++ src/recu_diaz_cliente/Recu_diaz_cliente.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package recu_diaz_cliente;
+
+/**
+ *
+ * @author hector0303
+ */
+public class Recu_diaz_cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
